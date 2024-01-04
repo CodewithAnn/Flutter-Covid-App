a4f59fa3dbdf878d730c2921bf07281f71c63ded
Anand jha <codewithann@gmail.com>
2024-01-05 02:02:08 +0530

    created animated SplashScreen

---
+++ lib/Screens/Home/worldStat.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WorldStat extends StatefulWidget {
+  const WorldStat({super.key});
+
+  @override
+  State<WorldStat> createState() => _WorldStatState();
+}
+
+class _WorldStatState extends State<WorldStat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/Screens/splashScreen.dart
+++ lib/Screens/splashScreen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:covid_app/Screens/Home/worldStat.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -7,9 +11,72 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _animationController =
+      AnimationController(vsync: this, duration: Duration(seconds: 3))
+        ..repeat();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+      Duration(seconds: 3.9.toInt()),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => WorldStat(),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // for size
+    var _mediaQuery = MediaQuery.of(context);
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          AnimatedBuilder(
+            animation: _animationController,
+            child: Container(
+              height: _mediaQuery.size.height * 0.80,
+              width: _mediaQuery.size.width * 1,
+              child: Center(
+                child: Image(
+                    image: AssetImage('assets/images/covid-animated.png')),
+              ),
+            ),
+            builder: (context, child) {
+              return Transform.scale(
+                scale: _animationController.value * 0.5,
+                child: child,
+              );
+            },
+          ),
+          SizedBox(
+            height: _mediaQuery.size.height * .02,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Covid-19\n \t App',
+              style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
