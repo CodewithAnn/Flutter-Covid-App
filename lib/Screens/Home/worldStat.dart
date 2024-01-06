ffd463d76fbb8edfc7ba4d6e0d3e68a67e9950ff
Anand jha <codewithann@gmail.com>
2024-01-06 23:20:23 +0530

    created pie chart
      - Design the Ui of World Stats Screens
      - That show the Data of Covid through pieChart
       - later we change the dummy data with Api

--- lib/Screens/Home/worldStat.dart
+++ lib/Screens/Home/worldStat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class WorldStat extends StatefulWidget {
   const WorldStat({super.key});
@@ -7,9 +8,45 @@ class WorldStat extends StatefulWidget {
   State<WorldStat> createState() => _WorldStatState();
 }
 
-class _WorldStatState extends State<WorldStat> {
+class _WorldStatState extends State<WorldStat> with TickerProviderStateMixin {
+  late final AnimationController _animationController =
+      AnimationController(vsync: this, duration: Duration(seconds: 3))
+        ..repeat();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
+
+  final _colorList = <Color>[
+    const Color(0xff4285F4),
+    const Color(0xff1aa260),
+    const Color(0xffde5246),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var _mediaQuery = MediaQuery.of(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: _mediaQuery.size.height * 0.01,
+            ),
+            PieChart(
+              dataMap: {"Total": 100, "Death": 68, "Recover": 32},
+              animationDuration: Duration(milliseconds: 1200),
+              chartType: ChartType.ring,
+              chartRadius: _mediaQuery.size.width / 3.6,
+              colorList: _colorList,
+              legendOptions: LegendOptions(
+                legendPosition: LegendPosition.left,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
