2da1085e112ef801e7f256ed105872c5a240a5d9
Anand jha <codewithann@gmail.com>
2024-01-07 08:15:36 +0530

    added Dummy Data
        - Added Card for Showing Dummy Data
        - Added Elevated Button for Tracking Countries Data
        - Later we replace Dummy Data with real Data (Api call) and show Countries data also

--- lib/Screens/Home/worldStat.dart
+++ lib/Screens/Home/worldStat.dart
@@ -44,9 +44,51 @@ class _WorldStatState extends State<WorldStat> with TickerProviderStateMixin {
                 legendPosition: LegendPosition.left,
               ),
             ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  vertical: _mediaQuery.size.height * 0.06,
+                  horizontal: _mediaQuery.size.width * 0.029),
+              child: Card(
+                child: Column(
+                  children: [
+                    _reUseableRow(title: "Total", value: "300"),
+                    _reUseableRow(title: "Total", value: "300"),
+                    _reUseableRow(title: "Total", value: "300"),
+                    _reUseableRow(title: "Total", value: "300"),
+                  ],
+                ),
+              ),
+            ),
+            // Elevated Button for Tracking Countries Data
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Track Country Data"),
+            ),
           ],
         ),
       ),
     );
   }
+
+// method for using row multiple times
+  Widget _reUseableRow({required String title, required String value}) =>
+      Padding(
+        padding: const EdgeInsets.only(left: 10, right: 8, top: 12, bottom: 9),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(title),
+                Text(value),
+              ],
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Divider(),
+          ],
+        ),
+      );
 }
