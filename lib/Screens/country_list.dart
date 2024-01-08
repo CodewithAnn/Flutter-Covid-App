0b2eb162de43d58322d95f6c43a40a9307d3721c
Anand jha <codewithann@gmail.com>
2024-01-09 00:46:05 +0530

    fetched country data
        - fetched country Name,Cases and Flag in Alphabetic order
        - implemented Ui and Api call
        - still yet to do add flitter country by name and shimmer effect in ui

--- lib/Screens/country_list.dart
+++ lib/Screens/country_list.dart
@@ -1,3 +1,4 @@
+import 'package:covid_app/services/utilliteis/stats_services.dart';
 import 'package:flutter/material.dart';
 
 class CountryList extends StatefulWidget {
@@ -8,8 +9,81 @@ class CountryList extends StatefulWidget {
 }
 
 class _CountryListState extends State<CountryList> {
+  TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _searchController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    StatServices _statServices = StatServices();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              onChanged: (value) {
+                setState(() {});
+              },
+              controller: _searchController,
+              decoration: InputDecoration(
+                hintText: "Search by Country name",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(23),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: FutureBuilder(
+              future: _statServices.fetchCountyList(),
+              builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          ListTile(
+                            title: Text(
+                              snapshot.data![index]['country'].toString(),
+                              style: const TextStyle(
+                                  fontSize: 19, fontWeight: FontWeight.w600),
+                            ),
+                            subtitle: Text(
+                              snapshot.data![index]['cases'].toString(),
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            leading: Image(
+                              height: 70,
+                              width: 70,
+                              image: NetworkImage(
+                                snapshot.data![index]['countryInfo']["flag"],
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    },
+                  );
+                } else {
+                  return Text('Loding');
+                }
+              },
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
