6b7168de14d365c3397e4f923652012d6661b79d
Anand jha <codewithann@gmail.com>
2024-01-08 23:36:07 +0530

    call  countryApi Data
        - call country_list without any Model data

--- lib/services/utilliteis/stats_services.dart
+++ lib/services/utilliteis/stats_services.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:covid_app/Screens/country_list.dart';
 import 'package:covid_app/model/covid_world_stats.dart';
 import 'package:covid_app/services/utilliteis/app_url.dart';
 import 'package:http/http.dart' as http;
@@ -13,6 +14,20 @@ class StatServices {
 
     if (response.statusCode == 200) {
       return CovidWorldStats.fromJson(data);
+    } else {
+      throw Exception('Error');
+    }
+  }
+
+  // fetch data county wise
+  final countyUri = Uri.parse(AppUrl.countryData);
+
+  Future<List<dynamic>> fetchCountyList() async {
+    final response = await http.get(countyUri);
+    var data = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      return data;
     } else {
       throw Exception('Error');
     }
